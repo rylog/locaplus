06ad4ad460527e2ae3d4925e6467cb25fd9d9214
rylog <ryanlomtl@gmail.com>
2025-02-17 15:00:45 -0500

    clean input in utils

--- src/app/api/quotes/route.ts
+++ src/app/api/quotes/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+import sanitizeHtml from 'sanitize-html';
 
 import { QuoteRequest } from '@/api/useSendQuoteRequest';
 
@@ -7,13 +8,21 @@ import { sendEmail } from '../services/emailService';
 import { isValidEmail } from '../utils/emailValidator';
 import { generateQuotesMessage } from '../utils/quotes';
 
+const cleanInput = (input: string) =>
+  sanitizeHtml(input, {
+    allowedTags: [], // No HTML allowed
+    allowedAttributes: {},
+  });
+
 export const POST = async (req: NextRequest) => {
   if (req.method !== 'POST') {
     return NextResponse.json({ error: 'Method Not Allowed' }, { status: 405 });
   }
 
   try {
-    const quoteRequest = (await req.json()) as QuoteRequest;
+    const rawBody = await req.text(); // Get raw JSON as a string
+    const sanitizedBody = cleanInput(rawBody); // Sanitize entire JSON string
+    const quoteRequest = JSON.parse(sanitizedBody) as QuoteRequest; // Parse into an object
     const locaplusEmail = process.env.LOCAPLUS_EMAIL;
 
     const { reCaptchaToken, recipient, language } = quoteRequest;
