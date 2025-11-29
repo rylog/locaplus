92e205ca0de36b23ec4dffb3f42163f3f7d89d5a
rylog <ryanlomtl@gmail.com>
2025-11-29 15:06:47 -0500

    optimize only first image

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'crypto';
 import type { CollectionConfig } from 'payload';
 
 export const Media: CollectionConfig = {
@@ -12,5 +13,28 @@ export const Media: CollectionConfig = {
       required: true,
     },
   ],
-  upload: true,
+  upload: {
+    displayPreview: true,
+  },
+  hooks: {
+    beforeValidate: [
+      ({ data, req }) => {
+        const file = req?.file;
+
+        if (file) {
+          const ext = file.name.split('.').pop();
+          const base = file.name.replace(`.${ext}`, '');
+
+          // Add a short random hash at the end
+          const hash = randomUUID().slice(0, 8); // 8-char hash
+          const newName = `${base}__${hash}.${ext}`;
+
+          file.name = newName;
+          if (data) data.filename = newName;
+        }
+
+        return data;
+      },
+    ],
+  },
 };
